3a1e841dd7b9263f3fff4326d2f13a2bcbab4578
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-04-30 20:25:37 -0300

    Change user password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'mysql',
     host: '127.0.0.1',
     username: 'root',
-    password: '123mudar',
+    password: '123Mud@r',
     database: 'gonodemodulo2',
     operatorAliases: false,
     define: {
