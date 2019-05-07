898a7c5f389f7c9a7500cb0ee50bbed946b6332e
rvbonfimm <rogeriovilelabonfim@gmail.com>
2019-05-07 19:47:22 -0300

    Omit the current user to create schedules for himself

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,8 +1,16 @@
 const { User } = require('../models')
+const { Op } = require('sequelize')
 
 class DashboardController {
     async index (req, res) {
-        const providers = await User.findAll({ where: { provider: true } })
+        const providers = await User.findAll({
+            where: {
+                id: {
+                    [Op.ne]: req.session.user.id
+                },
+                provider: true
+            }
+        })
 
         return res.render('dashboard', { providers })
     }
